299a3c7fb8d12757e46f6a65393ca461c55101a3
Allie LeFever <alefever39@gmail.com>
2022-05-16 18:04:49 -0600

    commit tree 94db7507a7e60198f72f2901dcd03ec697c04f4c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+    return (Math.abs(block - 42))*264;
+}
+
+function distanceTravelledInFeet(block1, block2) {
+    return (Math.abs(block1 - block2))*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const dis = distanceTravelledInFeet(start, destination);
+    let fare;
+    if (dis <= 400) {
+        fare = 0;
+    }
+    else if (dis > 400 && dis <= 2000) {
+        fare = (dis - 400) * .02;
+    }
+    else if (dis > 2000 && dis <= 2500) {
+        fare = 25;
+    }
+    else {
+        fare = 'cannot travel that far';
+    }
+    return fare;
+}
